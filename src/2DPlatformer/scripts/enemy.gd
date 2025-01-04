b739f83a38c0e377ba5f62c983393879c9a4207c
cheese <deanlemans5646@gmail.com>
2025-01-04 17:31:47 +0100

    update scene leaks

--- src/2DPlatformer/scripts/enemy.gd
+++ src/2DPlatformer/scripts/enemy.gd
@@ -5,4 +5,8 @@ var animation_player : AnimationPlayer
 
 func _on_area_2d_body_entered(body: Node2D):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://2DPlatformer/Scenes/lost.tscn")
+		# Delay the scene change until the physics callback is finished
+		call_deferred("change_scene")
+
+func change_scene():
+	get_tree().change_scene_to_file("res://2DPlatformer/Scenes/lost.tscn")
